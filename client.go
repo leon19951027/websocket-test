dbd76c5fbb0c675643f40795563f53e792fa35c2
zhengjiayang <zhengjiayang@adups.com>
2021-03-28 15:15:01 +0800

    up

--- client.go
+++ client.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
@@ -12,6 +15,15 @@ import (
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
+
+func getKeyborad(message chan string) {
+	for {
+		keyborad := bufio.NewReader(os.Stdin)
+		s1, _ := keyborad.ReadString('\n')
+		message <- s1
+	}
+
+}
 
 func main() {
 	flag.Parse()
@@ -23,14 +35,21 @@ func main() {
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws"}
 	log.Printf("connecting to %s", u.String())
 
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	header := make(http.Header)
+	fmt.Println("--------------------")
+	header.Add("AK", "r7HdGvhpC$dUn3Q")
+
+	log.Println(header)
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), header)
 	if err != nil {
 		log.Fatal("error:", err)
 	}
 
 	defer c.Close()
 
+	message := make(chan string)
 	done := make(chan struct{})
+	go getKeyborad(message)
 
 	go func() {
 		defer close(done)
@@ -49,10 +68,8 @@ func main() {
 
 	for {
 		select {
-		case <-done:
-			//return
-		case <-ticker.C:
-			err := c.WriteMessage(9, []byte("hello"))
+		case msg := <-message:
+			err := c.WriteMessage(2, []byte(msg))
 			if err != nil {
 				time.Sleep(1 * time.Second)
 				log.Println("write:", err)
@@ -60,17 +77,10 @@ func main() {
 			}
 		case <-interrupt:
 			log.Println("interrupt")
-
-			// Cleanly close the connection by sending a close message and then
-			// waiting (with timeout) for the server to close the connection.
 			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
 				log.Println("write close:", err)
 				return
-			}
-			select {
-			case <-done:
-			case <-time.After(time.Second):
 			}
 			return
 		}
--- server.go
+++ server.go
@@ -10,12 +10,32 @@ import (
 
 func main() {
 	g := gin.Default()
+	g.Use(MiddleJWT())
 	g.GET("/ws", StartWs)
+	g.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"code": "200"})
+	})
+
 	g.Run(":8080")
 }
 
+func MiddleJWT() gin.HandlerFunc {
+	log.Println("加载中间件JWT")
+	return func(c *gin.Context) {
+		if c.GetHeader("AK") != "r7HdGvhpC$dUn3Q" || c.GetHeader("AK") == "" {
+			log.Println("--------")
+			c.JSON(400, gin.H{"code": "400", "message": "token miss"})
+			c.Abort()
+			return
+		} else {
+
+		}
+		c.Next()
+	}
+}
+
 func StartWs(c *gin.Context) {
-	log.Println(c.Request.RemoteAddr)
+	//log.Println(c.Request.RemoteAddr, c.Request.RequestURI)
 	var upgrader = websocket.Upgrader{
 		// 解决跨域问题
 		CheckOrigin: func(r *http.Request) bool {
@@ -45,7 +65,7 @@ func read(c *websocket.Conn) {
 	for {
 		_, message, err := c.ReadMessage()
 		log.Println("client message", string(message), c.RemoteAddr())
-		c.WriteMessage(10, []byte("pong"))
+		c.WriteMessage(1, []byte("pong"))
 		if err != nil { // 离线通知
 			//	offline <- c
 			log.Println("ReadMessage error1", err)
